bcc7b61898b587fd4901dc8d8af8b07328052480
Mumus <weiszd@gmail.com>
2014-04-27 11:24:09 +0300

    Fix the PoW block number limit
    Approximated calculation of the total number of PoW block to match the 100,000,000 PoW coins anounced in thread https://bitcointalk.org/index.php?topic=578963.0
    Exact calculation should be made by the dev

--- src/main.h
+++ src/main.h
@@ -26,7 +26,7 @@ class CInv;
 class CRequestTracker;
 class CNode;
 
-static const int LAST_POW_BLOCK = 100000000;
+static const int LAST_POW_BLOCK = 4500000;
 
 static const unsigned int MAX_BLOCK_SIZE = 1000000;
 static const unsigned int MAX_BLOCK_SIZE_GEN = MAX_BLOCK_SIZE/2;
